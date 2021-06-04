ac00d716109b3ad4f2d7a7b6ddb3cb01568cce8a
Sheko1 <martinkypar@gmail.com>
2021-06-05 00:29:29 +0300

    added bootstrap, add field to add task, add finish and unfinish button for task state, add delete button to delete task

--- Python-Web/todo_project/todo_project/todo_app/urls.py
+++ Python-Web/todo_project/todo_project/todo_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from todo_project.todo_app.views import index
+from todo_project.todo_app.views import index, todo_add, todo_delete
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('todo_add', todo_add),
+    path('todo_delete/<int:pk>', todo_delete)
 ]
--- Python-Web/todo_project/todo_project/todo_app/views.py
+++ Python-Web/todo_project/todo_project/todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from todo_project.todo_app.models import Todo
@@ -7,3 +7,26 @@ from todo_project.todo_app.models import Todo
 def index(req):
     context = {'todos': Todo.objects.all()}
     return render(req, "index.html", context=context)
+
+
+def todo_add(req):
+    title = req.POST['title']
+    description = req.POST['description']
+    Todo(title=title, description=description).save()
+    return redirect('/')
+
+
+def todo_delete(req, pk):
+    todo = Todo.objects.get(pk=pk)
+    if "sheko" in req.POST.keys():
+        todo.is_done = True
+        todo.save()
+
+    elif 'cheko' in req.POST.keys():
+        todo.is_done = False
+        todo.save()
+
+    else:
+        todo.delete()
+
+    return redirect('/')
