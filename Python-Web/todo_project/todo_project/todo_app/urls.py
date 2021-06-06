a620caa4cafd956393ec581cd0b10ae3c68edf29
Sheko1 <martinkypar@gmail.com>
2021-06-06 10:25:56 +0300

    fixed urls

--- Python-Web/todo_project/todo_project/todo_app/urls.py
+++ Python-Web/todo_project/todo_project/todo_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from todo_project.todo_app.views import index, todo_add, todo_delete
+from todo_project.todo_app.views import index, todo_add, todo_change_status_or_delete
 
 urlpatterns = [
     path('', index),
     path('todo_add', todo_add),
-    path('todo_delete/<int:pk>', todo_delete)
+    path('todo_delete/<int:pk>', todo_change_status_or_delete)
 ]
